9cd696d889604747986116e7c8ca2507b4dbb191
Gowtham Elluru <gowthamelluruofficial@gmail.com>
2026-09-11 10:28:52 +0530

    Time: 80 ms (5.07%) | Memory: 47.9 MB (7.47%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i:digits){
+            hm.put(i,hm.getOrDefault(i,0)+1);
+        }
+        int ans = 0;
+        for(int j=100; j<=998; j+=2){
+            int i = j;
+            int a = i%10;
+            i/=10;
+            int b = i%10;
+            i/=10;
+            int c = i%10;
+            if(hm.containsKey(a)){
+                int val = hm.get(a);
+                if(val==1) hm.remove(a);
+                else hm.put(a,val-1);
+                if(hm.containsKey(b)){
+                    val = hm.get(b);
+                    if(val==1) hm.remove(b);
+                    else hm.put(b,val-1);
+                    if(hm.containsKey(c)){
+                        val = hm.get(c);
+                        if(val==1) hm.remove(c);
+                        else hm.put(c,val-1);
+                        ans++;
+                    }
+                }
+            }
+            hm = new HashMap<>();
+            for(int in:digits){
+                hm.put(in,hm.getOrDefault(in,0)+1);
+            }
+        }
+        return ans;
+    }
+}
