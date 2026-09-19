a6ff8f6688fbd65fe07fd0ec8eeaa6e077d8cc7c
Gowtham Elluru <gowthamelluruofficial@gmail.com>
2026-09-19 11:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (86.55%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
@@ -1,7 +1,15 @@
 class Solution {
     public boolean checkOverlap(int r, int x, int y, int x1, int y1, int x2, int y2) {
-        int closestX = Math.max(x1, Math.min(x, x2));
-        int closestY = Math.max(y1, Math.min(y, y2));
+        int closestX;
+        int closestY;
+        if (x >= x1 && x <= x2)
+            closestX = x;
+        else
+            closestX = Math.abs(x - x1) < Math.abs(x - x2) ? x1 : x2;
+        if (y >= y1 && y <= y2)
+            closestY = y;
+        else
+            closestY = Math.abs(y - y1) < Math.abs(y - y2) ? y1 : y2;
         int dx = x - closestX;
         int dy = y - closestY;
         return dx * dx + dy * dy <= r * r;
